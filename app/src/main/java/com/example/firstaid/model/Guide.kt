78301abef4ddfae5530442e4b2c5f30dd8f80d69
Max <sulimenko.maxim@gmail.com>
2025-03-13 11:03:40 +0300

    add tags for questioning

--- app/src/main/java/com/example/firstaid/model/Guide.kt
+++ app/src/main/java/com/example/firstaid/model/Guide.kt
@@ -1,12 +1,18 @@
 package com.example.firstaid.model
 
+data class Question(
+    val id: Int,
+    val text: String,
+    val tag: String
+)
+
 data class Guide(
     val id: Int,
     val title: String,
     val description: String,
     val steps: List<Step>,
+    val tags: List<String>
 )
-
 data class Step(
     val title: String,
     val items: List<PageItem>
