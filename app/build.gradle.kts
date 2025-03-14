3e5dd72ed769842a0d1aee20ebc11e3e3d7524d6
Andrei Malykh <xekoro10@gmail.com>
2025-03-14 17:09:35 +0300

    move dependencies to libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,11 +40,11 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("org.osmdroid:osmdroid-android:6.1.14")
-    implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation("com.google.android.material:material:1.8.0-alpha01")
-    implementation("androidx.compose.material3:material3:1.0.0-beta03")
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.osmdroid.android)
+    implementation(libs.play.services.location)
+    implementation(libs.material)
+    implementation(libs.material3)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
